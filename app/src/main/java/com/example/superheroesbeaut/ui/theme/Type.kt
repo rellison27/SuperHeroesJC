0796c78c5deb6a6023a505997406f6572de7c838
Rashaun Ellison <rellison27@gmail.com>
2023-11-08 07:55:56 -0500

    adding fonts and assets

--- app/src/main/java/com/example/superheroesbeaut/ui/theme/Type.kt
+++ app/src/main/java/com/example/superheroesbeaut/ui/theme/Type.kt
@@ -2,18 +2,33 @@ package com.example.superheroesbeaut.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.example.superheroesbeaut.R
 
+
+val Cabin = FontFamily(
+    Font(R.font.cabin_regular),
+    Font(R.font.cabin_bold)
+)
 // Set of Material typography styles to start with
 val Typography = Typography(
-    bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+    displayLarge = TextStyle(
+        fontFamily = Cabin,
         fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
+        fontSize = 36.sp
+    ),
+    labelSmall = TextStyle(
+        fontFamily = Cabin,
+        fontWeight = FontWeight.Bold,
+        fontSize = 14.sp
+    ),
+    bodyLarge = TextStyle(
+        fontFamily = Cabin,
+        fontWeight = FontWeight.Normal,
+        fontSize = 14.sp
     )
     /* Other default text styles to override
     titleLarge = TextStyle(
@@ -22,13 +37,5 @@ val Typography = Typography(
         fontSize = 22.sp,
         lineHeight = 28.sp,
         letterSpacing = 0.sp
-    ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
-    )
-    */
+    ),*/
 )
