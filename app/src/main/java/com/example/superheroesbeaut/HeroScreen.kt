18be5ec38dcb91ae81adf6ecb69afda61a75439b
Rashaun Ellison <rellison27@gmail.com>
2023-11-10 17:20:38 -0500

    app fully completed with dark theme and light theme looking good

--- app/src/main/java/com/example/superheroesbeaut/HeroScreen.kt
+++ app/src/main/java/com/example/superheroesbeaut/HeroScreen.kt
@@ -3,10 +3,14 @@ package com.example.superheroesbeaut
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
@@ -22,56 +26,63 @@ import com.example.superheroesbeaut.model.Hero
 import com.example.superheroesbeaut.model.HeroRepositiory
 import com.example.superheroesbeaut.ui.theme.SuperHeroesBeautTheme
 
-class HeroScreen
-{
-    private val heroRepository = HeroRepositiory
+private val heroRepository = HeroRepositiory
 
-    @Composable
-    fun HeroCard(
-        modifier: Modifier = Modifier,
-        hero: Hero
-    ) {
-        Card(
-            elevation = CardDefaults.cardElevation(2.dp),
-            modifier = Modifier.clip(MaterialTheme.shapes.medium)
-        ) {
-            Box(modifier = Modifier
-                .padding(16.dp)
+@Composable
+fun HeroList(modifier: Modifier = Modifier, heroList: List<Hero>, contentPaddingValues: PaddingValues)
+{
+    LazyColumn(contentPadding = contentPaddingValues ) {
+        items(items = heroList) {
+            HeroCard(
+                hero = it,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(start = 16.dp, end = 16.dp, bottom = 8.dp)
             )
-            {
-                Row (
-                    modifier = Modifier.height(72.dp)
-                ){
-                    Column(modifier = Modifier.padding(end = 16.dp)) {
-                        Text(
-                            text = stringResource(id = hero.heroName),
-                            style = MaterialTheme.typography.displaySmall
-                        )
-                        Text(
-                            text = stringResource(id = hero.powersDescription),
-                            style = MaterialTheme.typography.bodyLarge
-                        )
-                    }
-                    Box(modifier = Modifier) {
-                        Image(
-                            modifier = Modifier
-                                .clip(MaterialTheme.shapes.small)
-                                .size(72.dp),
-                            painter = painterResource(id = hero.imageResId),
-                            contentDescription = stringResource(id = hero.powersDescription)
-                        )
-                    }
-                }
-            }
         }
     }
+}
 
-    @Preview(showBackground = true)
-    @Composable
-    fun SuperHeroCard()
-    {
-        SuperHeroesBeautTheme {
-            HeroCard(modifier = Modifier, hero = heroRepository.heroes[0])
+@Composable
+fun HeroCard(
+    modifier: Modifier = Modifier,
+    hero: Hero
+) {
+    Card(
+        elevation = CardDefaults.cardElevation(2.dp),
+        modifier = modifier.clip(MaterialTheme.shapes.medium)
+    ) {
+        Box(modifier = Modifier
+            .padding(16.dp)
+        )
+        {
+            Row (
+                modifier = Modifier.height(72.dp)
+            ){
+                Column(
+                    modifier = Modifier
+                        .padding(end = 16.dp)
+                        .weight(1F)
+                ) {
+                    Text(
+                        text = stringResource(id = hero.heroName),
+                        style = MaterialTheme.typography.displaySmall
+                    )
+                    Text(
+                        text = stringResource(id = hero.powersDescription),
+                        style = MaterialTheme.typography.bodyLarge
+                    )
+                }
+                Box(modifier = Modifier) {
+                    Image(
+                        modifier = Modifier
+                            .clip(MaterialTheme.shapes.small)
+                            .size(72.dp),
+                        painter = painterResource(id = hero.imageResId),
+                        contentDescription = stringResource(id = hero.powersDescription)
+                    )
+                }
+            }
         }
     }
 }
--- app/src/main/java/com/example/superheroesbeaut/MainActivity.kt
+++ app/src/main/java/com/example/superheroesbeaut/MainActivity.kt
@@ -3,12 +3,20 @@ package com.example.superheroesbeaut
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.superheroesbeaut.model.HeroRepositiory
 import com.example.superheroesbeaut.ui.theme.SuperHeroesBeautTheme
 
 class MainActivity : ComponentActivity()
@@ -30,11 +38,35 @@ class MainActivity : ComponentActivity()
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun SuperHeroApp(modifier: Modifier = Modifier)
 {
-
+    Scaffold(
+        topBar = {
+            SuperHeroesTopAppBar()
+        }
+    ) {it ->
+        HeroList(heroList = HeroRepositiory.heroes, contentPaddingValues = it)
+    }
 }
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun SuperHeroesTopAppBar(modifier: Modifier = Modifier) {
+    CenterAlignedTopAppBar(
+        modifier = modifier,
+        title = {
+            Row(verticalAlignment = Alignment.CenterVertically) {
+                Text(
+                    text = stringResource(id = R.string.app_name),
+                    style = MaterialTheme.typography.displayLarge
+                )
+            }
+        }
+    )
+}
+
 
 @Preview(showBackground = true)
 @Composable
